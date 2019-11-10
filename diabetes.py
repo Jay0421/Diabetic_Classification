1975d49cdb2d6ab39fa81c06bc7b2a473a14318e
Jay <jayaramakrishnan.jrk@gmail.com>
2019-11-10 21:48:32 +0000

    Load dataset

--- diabetes.py
+++ diabetes.py
@@ -10,18 +10,3 @@ from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv("diabetes.csv")
 data.head()
-
-# Handling Missing Values
-
-data.isnull().sum()
-data = data.replace(0, np.nan)
-data = data.fillna(data.mean())
-
-# Label Encoding
-
-label = LabelEncoder()
-data["is_diabetic"] = label.fit_transform(data["is_diabetic"])
-data.head()
-
-# Train Test Split
-
