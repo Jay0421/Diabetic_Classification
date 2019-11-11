887ab6e3cdf86d1f28bea12b1f6172f94aea6979
Jay <jayaramakrishnan.jrk@gmail.com>
2019-11-11 15:20:19 +0000

    Prediction

--- diabetes.py
+++ diabetes.py
@@ -39,3 +39,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 classifier = RandomForestClassifier()
 classifier.fit(x_train, y_train)
 
+# Prediction
+
+y_pred = classifier.predict(x_test)
